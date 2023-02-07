9de362a5400c45d0bb23272ee1e3b8dc2f4999f9
ayx99 <amanyadavx99@gmail.com>
2023-02-07 23:38:29 +0530

    added uninstaller script to remove games and non required apps of gnome

---
+++ uninstaller.sh
@@ -0,0 +1,3 @@
+#Script to uninstall basic games and apps that are usually not required
+sudo apt purge gnome-2048 aisleriot atomix gnome-chess gnome-contacts five-or-more hitori iagno gnome-klotski lightsoff gnome-mahjongg gnome-maps gnome-mines gnome-nibbles quadrapassel four-in-a-row gnome-robots gnome-sudoku swell-foop tali gnome-taquin gnome-tetravex evolution gnome-weather 
+
